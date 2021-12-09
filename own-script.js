972371f9fe0c46d274818474b58f8f78704b746a
BlackyAranga <48901640+BlackyAranga@users.noreply.github.com>
2021-12-09 21:13:52 +0530

    Update own-script.js

--- own-script.js
+++ own-script.js
@@ -1,23 +1,5 @@
+$(document).ready(function(){
 
-document.addEventListener('DOMContentLoaded',() => {
-    
+    console.log("Working");
 
-    var codeArray = document.getElementsByTagName('code');
-    var codeArrayCount = codeArray.length;
-    if(codeArrayCount != 0){
-        for(var i =0; i < codeArrayCount; i++){
-            var codeData = codeArray[i].innerHTML;
-            codeData = codeData.replace(/&lt;(.*?)&gt;/g,"<span class='code-element'>&lt;$1&gt;</span>");
-            
-            codeData = codeData.replace(/"(.*?)"/g,"<span class='code-str'>&quot;$1&quot;</span>");
-            
-            
-            codeData = codeData.replace(/\/\* (.*?) \*\//g,'<span class="code-cmt">/* $1 */</span>');
-            
-            
-            codeArray[i].innerHTML = codeData;
-        }
-    }
-    
-    
 });
